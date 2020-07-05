b62b221b2238381de4de0e7351d941074d366a12
sakthivel9963 <sakthivel9963@gmail.com>
2020-07-05 20:26:08 +0530

    :tada: added all the basic queries to default config

--- src/api/example/example.queries.js
+++ src/api/example/example.queries.js
@@ -1,29 +1,25 @@
-const connection = require('../../config/db');
+const DefaultQueries = require('../default/default.queries');
+// const connection = require('../../config/db');
 
-class ExampleQueries {
-  static find(tableName, whereObj = {}) {
-    return connection.table(tableName).where(whereObj);
-  }
-
-  static findOne(tableName, whereObj) {
-    return connection.table(tableName).where(whereObj);
-  }
-
-  static count(tableName) {
-    return connection.table(tableName).count('*', { as: 'count' });
-  }
-
-  static save(tableName, saveObj) {
-    return connection.table(tableName).insert(saveObj);
-  }
-
-  static update(tableName, updateObj, whereObj) {
-    return connection.table(tableName).where(whereObj).update(updateObj);
-  }
-
-  static delete(tableName, deleteObj) {
-    return connection.table(tableName).where(deleteObj).delete();
-  }
+class ExampleQueries extends DefaultQueries {
+  // static find(tableName, whereObj = {}) {
+  //   return connection.table(tableName).where(whereObj);
+  // }
+  // static findOne(tableName, whereObj) {
+  //   return connection.table(tableName).where(whereObj);
+  // }
+  // static count(tableName) {
+  //   return connection.table(tableName).count('*', { as: 'count' });
+  // }
+  // static save(tableName, saveObj) {
+  //   return connection.table(tableName).insert(saveObj);
+  // }
+  // static update(tableName, updateObj, whereObj) {
+  //   return connection.table(tableName).where(whereObj).update(updateObj);
+  // }
+  // static delete(tableName, deleteObj) {
+  //   return connection.table(tableName).where(deleteObj).delete();
+  // }
 }
 
 module.exports = ExampleQueries;
